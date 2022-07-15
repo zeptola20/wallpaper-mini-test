ccb7bdbcb2a0aff2302e369ef151d9ca1497c48f
zeptola20 <mehdi.sarmadi2019@gmail.com>
2022-07-15 19:46:51 +0430

    fix search box

--- lib/Provider/wallpaper_Provider.dart
+++ lib/Provider/wallpaper_Provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:unsplash/models/wallpaper.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,51 +17,24 @@ class WallpaperProvider with ChangeNotifier {
       notifyListeners();
       var url = Uri.parse('https://api.pexels.com/v1/curated');
       var response = await http.get(url, headers: {'Authorization': _apiKey});
-      var jsonData = jsonDecode(response.body);
       wallpaperModel = WallpaperModel.fromJson(json.decode(response.body));
       loading = false;
       notifyListeners();
     } catch (e) {
-      print('we have big errror');
-      print(e);
+      throw e;
     }
   }
 
-  // void searchWallpaper(String item) async {
-  //   wallpaperList = [];
-  //   var url = Uri.parse('https://api.pexels.com/v1/search?query=$item');
-  //   var response =
-  //       await http.get(url, headers: {'Authorization': _apiKey}).then((value) {
-  //     var jsonData = jsonDecode(value.body);
+  Future<void> searchWallpaper(String item) async {
+    try {
+      loading = true;
+      notifyListeners();
+      var url = Uri.parse('https://api.pexels.com/v1/search?query=$item');
+      var response = await http.get(url, headers: {'Authorization': _apiKey});
 
-  //     List data = jsonData['photos'];
-
-  //     for (var i = 0; i < data.length; i++) {
-  //       addWallpaper(WallpaperModel(
-  //         photographer: data[i]['photographer'],
-  //         photographer_url: data[i]['photographer_url'],
-  //         photographer_id: data[i]['photographer_id'],
-  //         src: SrcModel(
-  //           original: data[i]['src']['original'],
-  //           large2x: data[i]['src']['large2x'],
-  //           large: data[i]['src']['large'],
-  //           medium: data[i]['src']['medium'],
-  //           small: data[i]['src']['small'],
-  //           portrait: data[i]['src']['portrait'],
-  //           landscape: data[i]['src']['landscape'],
-  //           tiny: data[i]['src']['tiny'],
-  //         ),
-  //         liked: data[i]['liked'] ?? false,
-  //         alt: data[i]['alt'],
-  //       ));
-  //     }
-  //     wallpaperList = _wallpaperList;
-  //     loading = false;
-  //     notifyListeners();
-  //     print('***************************************************************');
-  //     print(wallpaperList.length);
-  //   });
-
-  //   print('mehdi:${wallpaperList.length}');
-  // }
+      wallpaperModel = WallpaperModel.fromJson(json.decode(response.body));
+      loading = false;
+      notifyListeners();
+    } catch (e) {}
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,6 +20,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var wi = MediaQuery.of(context).size.width;
+    var he = MediaQuery.of(context).size.height;
     var categoryData = Provider.of<CategoryProvider>(context);
     var wallpaperData = Provider.of<WallpaperProvider>(context, listen: true);
     var appBar2 = AppBar(
@@ -44,7 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Consumer<WallpaperProvider>(
             builder: ((context, value, child) => Column(
                   children: [
-                    //  searchBox(),
+                    searchBox(),
                     //   catWidget(categoryData: categoryData),
                     value.loading
                         ? const Center(
@@ -53,51 +55,62 @@ class _HomeScreenState extends State<HomeScreen> {
                               child: CircularProgressIndicator(),
                             ),
                           )
-                        : Expanded(
-                            child: Container(
-                                margin:
-                                    const EdgeInsets.only(top: 10, bottom: 10),
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 16),
-                                child: GridView.builder(
-                                    shrinkWrap: true,
-                                    gridDelegate:
-                                        SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount:
-                                          Platform.isWindows ? 3 : 2,
-                                      childAspectRatio: .6,
-                                      mainAxisSpacing: 10,
-                                      crossAxisSpacing: 10,
-                                    ),
-                                    itemCount:
-                                        value.wallpaperModel?.photos?.length ??
+                        :Expanded(
+                                child: Container(
+                                    margin: const EdgeInsets.only(
+                                        top: 10, bottom: 10),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16),
+                                    child: GridView.builder(
+                                        shrinkWrap: true,
+                                        gridDelegate:
+                                            SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount:
+                                              Platform.isWindows ? 3 : 2,
+                                          childAspectRatio: .6,
+                                          mainAxisSpacing: 10,
+                                          crossAxisSpacing: 10,
+                                        ),
+                                        itemCount: value.wallpaperModel?.photos
+                                                ?.length ??
                                             0,
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        decoration: BoxDecoration(
-                                            boxShadow: [
-                                              BoxShadow(
-                                                  color: Colors.grey.shade300,
-                                                  offset: const Offset(2, 2),
-                                                  blurRadius: 1)
-                                            ],
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
-                                        child: ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            child: Image.network(
-                                              value
-                                                  .wallpaperModel!
-                                                  .photos![index]
-                                                  .src!
-                                                  .portrait!,
-                                              fit: BoxFit.cover,
-                                              filterQuality: FilterQuality.high,
-                                            )),
-                                      );
-                                    })),
-                          )
+                                        itemBuilder: (context, index) {
+                                          return value.wallpaperModel!.photos!
+                                                  .isEmpty
+                                              ? const Center(
+                                                  child: Text('Nothing found!'),
+                                                )
+                                              : Container(
+                                                  decoration: BoxDecoration(
+                                                      boxShadow: [
+                                                        BoxShadow(
+                                                            color: Colors
+                                                                .grey.shade300,
+                                                            offset:
+                                                                const Offset(
+                                                                    2, 2),
+                                                            blurRadius: 1)
+                                                      ],
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              10)),
+                                                  child: ClipRRect(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              10),
+                                                      child: Image.network(
+                                                        value
+                                                            .wallpaperModel!
+                                                            .photos![index]
+                                                            .src!
+                                                            .portrait!,
+                                                        fit: BoxFit.cover,
+                                                        filterQuality:
+                                                            FilterQuality.high,
+                                                      )),
+                                                );
+                                        })),
+                              )
                   ],
                 ))
 
@@ -106,47 +119,49 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-// class searchBox extends StatefulWidget {
-//   searchBox({
-//     Key? key,
-//   }) : super(key: key);
+class searchBox extends StatefulWidget {
+  searchBox({
+    Key? key,
+  }) : super(key: key);
 
-//   @override
-//   State<searchBox> createState() => _searchBoxState();
-// }
+  @override
+  State<searchBox> createState() => _searchBoxState();
+}
 
-// class _searchBoxState extends State<searchBox> {
-//   var item = TextEditingController();
+class _searchBoxState extends State<searchBox> {
+  var item = TextEditingController();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.symmetric(horizontal: 24),
-//       margin: const EdgeInsets.symmetric(horizontal: 24),
-//       decoration: BoxDecoration(
-//         color: const Color(0xfff5f8fd),
-//         borderRadius: BorderRadius.circular(16),
-//       ),
-//       child: TextField(
-//         controller: item,
-//         onSubmitted: (_) {
-//           setState(() {
-//             Provider.of<WallpaperProvider>(context, listen: false)
-//                 .searchWallpaper(item.text);
-//           });
-//         },
-//         keyboardType: TextInputType.url,
-//         maxLines: 1,
-//         showCursor: true,
-//         cursorColor: const Color(0xFF05a081),
-//         decoration: const InputDecoration(
-//             prefixIcon: Icon(Icons.search),
-//             hintText: 'Search',
-//             border: InputBorder.none),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      margin: const EdgeInsets.symmetric(horizontal: 24),
+      decoration: BoxDecoration(
+        color: const Color(0xfff5f8fd),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: TextField(
+        maxLength: 25,
+        controller: item,
+        onSubmitted: (_) {
+          setState(() {
+            Provider.of<WallpaperProvider>(context, listen: false)
+                .searchWallpaper(item.text);
+          });
+        },
+        keyboardType: TextInputType.url,
+        maxLines: 1,
+        showCursor: true,
+        cursorColor: const Color(0xFF05a081),
+        decoration: InputDecoration(
+            counter: Container(),
+            prefixIcon: Icon(Icons.search),
+            hintText: 'Search',
+            border: InputBorder.none),
+      ),
+    );
+  }
+}
 
 // class catWidget extends StatelessWidget {
 //   const catWidget({
